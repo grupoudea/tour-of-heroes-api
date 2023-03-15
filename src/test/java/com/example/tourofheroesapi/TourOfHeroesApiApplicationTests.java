62fb01aa9d077ac818220cd9522204db906726d7
berracode <juanguillermoalarcon@gmail.com>
2023-03-14 21:58:32 -0500

    primer commit

---
+++ src/test/java/com/example/tourofheroesapi/TourOfHeroesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourofheroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOfHeroesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
