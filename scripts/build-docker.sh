669be09fd09977aaf2a5db96e9389b90b0fbc1e5
berracode <juanguillermoalarcon@gmail.com>
2023-03-17 18:57:43 -0500

    se dockeriza api de tour of heroes

---
+++ scripts/build-docker.sh
@@ -0,0 +1,32 @@
+echo "Starting..."
+pwd
+IMAGE_NAME="tour-of-heroes-api-i"
+CONTAINER_NAME="$IMAGE_NAME-con"
+
+echo $IMAGE_NAME
+echo $CONTAINER_NAME
+
+echo "Stoping container $CONTAINER_NAME"
+if docker stop $CONTAINER_NAME 2>&1 | grep -q "No such"; then
+  echo "Error trying stop container $CONTAINER_NAME maybe container does not exist"
+else
+  echo "$CONTAINER_NAME has been stopped. Good job!"
+fi
+
+echo "Removing container $CONTAINER_NAME"
+if docker rm $CONTAINER_NAME 2>&1 | grep -q "No such"; then
+  echo "Error trying remove container $CONTAINER_NAME maybe container does not exist"
+else
+  echo "$CONTAINER_NAME has been removed. Good job!"
+fi
+
+echo "Removing image $IMAGE_NAME"
+if docker rmi $IMAGE_NAME 2>&1 | grep -q "No such"; then
+  echo "Error trying remove image $IMAGE_NAME maybe image does not exist"
+else
+  echo "$IMAGE_NAME has been removed. Good job!"
+fi
+
+./gradlew clean build -x test
+
+docker build --tag $IMAGE_NAME .
---
+++ scripts/docker-start.sh
@@ -0,0 +1,2 @@
+docker run -e PASS_DB=${MY_PASS_DB} -e USER_DB=${MY_USER_DB} -e HOST_DB=${MY_HOST_DB} \
+--name tour-of-heroes-api-i-con -d -p 8080:8080 tour-of-heroes-api-i:latest
